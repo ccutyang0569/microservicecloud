019c57c53915e144ce92e3bcfa568c27334ca98a
yangming <ccutyang0569@sina.com>
2018-06-25 21:35:26 +0800

    feign启动类

---
+++ microservicecloud-api/src/main/java/com/founderbn/springcloud/service/DeptClientService.java
@@ -0,0 +1,10 @@
+package com.founderbn.springcloud.service;
+
+/**
+ * @program: microservicecloud
+ * @description:
+ * @author: yangming
+ * @create: 2018-06-24 22:51
+ **/
+public interface DeptClientService {
+}
